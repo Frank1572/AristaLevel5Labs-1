e0dc871b896fedc4b7bbdcf1d1100b74ba77a7f3
D062749 <robert.ullmann@sap.com>
2020-10-19 10:59:05 +0200

    removed checkout of "fabric-onboard"

--- dep_hec01.sh
+++ dep_hec01.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Updating from repo"
-git checkout fabric-onboard && git pull
+git pull
 echo "Generating all Configlets and uploading them to HEC01"
 ansible-playbook -i cvp cvp.yml -l hec01 --tags "configlets, upload_configlets"

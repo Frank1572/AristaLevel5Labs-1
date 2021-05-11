250be663a2625583c7b45c50f0b5fca4488d25ea
D062749 <robert.ullmann@sap.com>
2021-05-11 13:52:18 +0200

    removed unnecessary files

--- dep_hec14.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Updating from repo"
-git pull
-echo "Generating all Configlets and uploading them to HEC14"
-ansible-playbook -i cvp cvp.yml -l hec14 --tags "configlets, upload_configlets"
